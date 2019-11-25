df7a17a35f16cc4a1704a278edf2d84eb3d31010
tony13game <48582680+tony13game@users.noreply.github.com>
2019-11-25 10:33:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-app = dash.Dash()
+app = dash.Dash(__name__)
+server = app.server
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
